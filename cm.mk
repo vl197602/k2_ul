4e4d639969508527f71b7900308dd9e723c55950
vl197602 <radarga@gmail.com>
2013-10-03 07:31:57 +0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,8 +1,15 @@
 # Release name
-PRODUCT_RELEASE_NAME := k2ul
+PRODUCT_RELEASE_NAME := k2_ul
 
 # Inherit device configuration
 $(call inherit-product, device/htc/k2ul/device_k2ul.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := k2_ul
@@ -10,3 +17,6 @@ PRODUCT_NAME := cm_k2_ul
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := k2_ul
 PRODUCT_MANUFACTURER := htc
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_k2_ul BUILD_FINGERPRINT=euro/htc/k2ul:4.2.2/JB_MR2/573038:user/user-debug PRIVATE_BUILD_DESC="k2_ul-user 4.2.2 JB_MR2 573038 user-debug"
+
